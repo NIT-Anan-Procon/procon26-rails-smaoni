04f0d30137ea4f70893dfd74eb4d7ae6fc01ba58
Mori Kohki <1122320@st.anan-nct.ac.jp>
2015-03-16 13:26:49 +0900

    created onigokko action

--- app/controllers/opi_controller.rb
+++ app/controllers/opi_controller.rb
@@ -1,7 +1,6 @@
 class OpiController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
-
   def receiption
     user = User.find_by_email(params[:account])
     if user.valid_password?(params[:password])
@@ -37,8 +36,58 @@ class OpiController < ApplicationController
 
 
   def onigokko
-      data = [:account,:password,:x,:y].map{|sym| params[sym]}
-     #    = JSON.parse(data)
+    user = User.find_by_email(params[:account])
+    if user.valid_password?(params[:password])
+       # update a player
+       player.name = parms[:name]
+       player.x = params[:x].to_i
+       player.y = params[:y].to_i
+       player.save!
+       
+       # game logic
+       ## detect new oni
+       oni = Player.where(is_in_room: true, is_oni: true).first
+       ## get candidates except oni
+       candidates = Player.where(is_in_room: true, is_oni: false,
+           is_invisible: false, x: oni.x, y: oni.y).all
+       unless candidates.empty?
+         new_oni = candidates[rand(candidates.length)]
+         new_oni.is_oni = true
+         oni.is_oni = false
+         t = TIme.now + 10.minutes
+         (candidates+[oni)].select{|c|!c.is_oni}.each{|c|
+           c.is_invisible = true
+           c.invisible_time = t
+         }
+         candidates.each{|c| c.save }
+         oni.save
+       end
+       
+       ## check invisible time
+       Player.where(is_in_room: true).all.each{|p|
+         if p.invisible_time && p.invisible_time > Time.now
+           p.is_invisible = false
+           p.invisible_time = nil
+           p.save
+         end
+       }
+
+       ## score bornus
+#       Player.where(is_in_room: true,is_oni:false).all.
+       # get players
+       players = Player.where(is_in_room: true).all.map{|p|
+         {
+           account: p.user.email,
+           name: p.name,
+           x: p.x, y: p.y,
+           is_oni: p.is_oni, is_invisible: p.is_invisible,
+           score: p.score
+         }
+       }
+       render json: players
+    else
+       render json: { result: "failed pass"}
+    end
   end
 
   def post_comment
